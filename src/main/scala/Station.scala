f1b564b74ded15e6d3f799b3f613dfc0981c85ec
Maerodrim <stanislav.sergynin@gmail.com>
2021-12-13 11:08:09 +0400

    Initial commit

---
+++ src/main/scala/Station.scala
@@ -0,0 +1,10 @@
+case class Station(
+                    stationId: Integer,
+                    name: String,
+                    lat: Double,
+                    long: Double,
+                    dockcount: Integer,
+                    landmark: String,
+                    installation: String,
+                    notes: String
+                  )
